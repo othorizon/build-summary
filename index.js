59f8e12911125056741ff2a0e45d39382b3652a5
zhanghao <zhanghao6@kingsoft.com>
2018-03-14 02:08:48 +0800

    test

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const fs = require('fs')
 
 module.exports = {
     hooks: {
-        output: function () {
+        init: function () {
             const root = this.resolve('')
             console.log("prcess generate summary ,root: " + root);
             //直接调用命令
-            process.execSync(root + "/node_modules/gitbook-plugin-build-summary/buildsummary.sh \"" + root + "\"",
-                function (error, stdout, stderr) {
-                    console.log(stdout);
-                    if (error !== null) {
-                        // console.log('stdout: ' + stdout);
-                        console.log('stderr: ' + stderr);
-                        console.log('exec error: ' + error);
-                    }
-                });
-            // fs.writeFileSync(`${root}/SUMMARY.md`, "etest", { encoding: 'utf8' })
-            process.exec("ls -a&&cat " + root + "/SUMMARY.md",
+            // process.execSync(root + "/node_modules/gitbook-plugin-build-summary/buildsummary.sh \"" + root + "\"",
+            //     function (error, stdout, stderr) {
+            //         console.log(stdout);
+            //         if (error !== null) {
+            //             // console.log('stdout: ' + stdout);
+            //             console.log('stderr: ' + stderr);
+            //             console.log('exec error: ' + error);
+            //         }
+            //     });
+            fs.writeFileSync(`${root}/README.md`, "etest", { encoding: 'utf8' })
+            process.exec("ls -l&&cat " + root + "/SUMMARY.md",
                 function (error, stdout, stderr) {
                     console.log(stdout);
                     if (error !== null) {
