d655de7655ea11400d35a7bd65fb48d4ae11e904
zhanghao <zhanghao6@kingsoft.com>
2018-03-14 01:01:28 +0800

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
             const root = this.resolve('')
             console.log("prcess generate summary ,root: " + root);
             //直接调用命令
-            process.execSync(root+"/node_modules/gitbook-plugin-build-summary/buildsummary.sh \""+root+"\"",
+            process.exec(root+"/node_modules/gitbook-plugin-build-summary/buildsummary.sh \""+root+"\"",
                 function (error, stdout, stderr) {
                     console.log(stdout);
                     if (error !== null) {
@@ -16,18 +16,6 @@ module.exports = {
                         console.log('exec error: ' + error);
                     }
                 });
-                fs.writeFileSync( `${root}/SUMMARY.md`, "etest", { encoding: 'utf8' } )
-                process.exec("cat "+root+"/SUMMARY.md",
-                function (error, stdout, stderr) {
-                    console.log(stdout);
-                    if (error !== null) {
-                        // console.log('stdout: ' + stdout);
-                        console.log('stderr: ' + stderr);
-                        console.log('exec error: ' + error);
-                    }
-                });
-                console.log("prcess generate finished");
-            
         }
     }
 }
